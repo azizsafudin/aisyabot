d1f7d0b46d9cbc7a9534767f50e000e3de550369
xmodulus <azizsafudin@yahoo.com.sg>
2017-06-19 14:19:19 +0800

    Added new intents

--- routes/botman.php
+++ routes/botman.php
@@ -8,12 +8,21 @@ $middleware = Wit::create(env('WIT_AI_ACCESS_TOKEN'));
 
 $botman->hears('salam', function(Botman $bot){
     $bot->reply("Wa'alaikumussalam! How can I help you today?");
-});
+})->middleware($middleware);
 
 $botman->hears('get_random_number', function(Botman $bot){
     $bot->reply("Sure, is ".rand()." ok?");
-});
+})->middleware($middleware);
 
+$botman->hears('who_am_i', function(Botman $bot){
+    $user = $bot->userStorage()->get();
+
+    if ($user->has('name')) {
+        $bot->reply('You are '.$user->get('name'));
+    } else {
+        $bot->reply('I do not know you yet.');
+    }
+})->middleware($middleware);
 
 //for now start_conversation and set_intro does the same thing. Change in the future.
 $botman->hears('start_conversation', BotManController::class.'@introConversation')->middleware($middleware);
