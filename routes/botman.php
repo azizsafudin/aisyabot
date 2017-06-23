de78420f934fafa5b3bb131f34ca0626a58afa26
xmodulus <azizsafudin@yahoo.com.sg>
2017-06-23 21:29:29 +0800

    Removed all group commands

--- routes/botman.php
+++ routes/botman.php
@@ -34,7 +34,7 @@ $botman->hears('who_am_i', function(Botman $bot){
     }
 })->middleware($middleware);
 
-$botman->hears("my_name_is", function (BotMan $bot) {
+$botman->hears("set_name", function (BotMan $bot) {
     $bot->userStorage()->delete('name');
 
     $extras   = $bot->getMessage()->getExtras();
@@ -53,71 +53,17 @@ $botman->hears("my_name_is", function (BotMan $bot) {
     $bot->reply('I will call you '.$name);
 })->middleware($middleware);
 
-
+$botman->hears('get_bot_info', function(BotMan $bot){
+        $bot->reply('My name is '.env('BOT_NAME'));
+        $bot->typesAndWaits(3);
+        $bot->reply('I was created by Abdul Aziz.');
+        $bot->reply('Also known as, @modulus');
+});
 //This block of commands are good old fashioned bot commands
 
-$botman->hears("/forgetme@".env('BOT_NAME'), function(Botman $bot){
+$botman->hears("forget_me", function(Botman $bot){
     $bot->userStorage()->delete();
     $bot->reply("Ok, I've forgotten everything about you.");
-});
-
-$botman->hears("/myid@".env('BOT_NAME'), function(Botman $bot){
-    $user = $bot->userStorage()->get();
-    $userid = $user->get('id');
-    if(!isset($userid) || empty($userid) || is_null($userid)) {
-        $bot->reply('Your ID has not been set.');
-        $bot->replyPrivate('Say: My name is ____.');
-    }
-    else{
-        $bot->reply('Your ID is: '.$userid);
-    }
-});
-
-
-$botman->hears('/setcountdown@'.env('BOT_NAME').' {name} {date}', function(Botman $bot, $name, $date){
-    $user = $bot->getUser();
-    $now = Carbon::createFromFormat('Y-m-d', Carbon::now()->addDays(rand(1,5)));
-
-    if (Carbon::createFromFormat('Y-m-d', $date) == false) {
-        $bot->reply('Sorry, but I don\'t understand that format.');
-        $bot->typesAndWaits(5);
-        $bot->replyPrivate('Try using the format Y-m-d, like so: '.$now);
-    }else{
-        $bot->userStorage()->save([
-            'countdown_name' => $name,
-            'countdown_date' => $date,
-        ]);
-        $bot->reply('I have saved that countdown, ' . $user->getFirstName() . '!');
-    }
-});
-
-$botman->hears('/countdown@'.env('BOT_NAME'), function(Botman $bot){
-    $user    = $bot->userStorage()->get();
-    $date    = Carbon::createFromFormat('Y-m-d', $user->get('countdown_date'));
-    $diff    = Carbon::now()->diffInDays($date);
-    $name    = $user->get('countdown_name');
-
-    if($diff > 0) {
-        $bot->reply($name . ' will begin in ' . $diff . ' days!');
-    }elseif($diff < 0){
-        $bot->reply($name. ' ended '.$diff.' days ago!');
-        $bot->userStorage()->delete('countdown_name');
-        $bot->userStorage()->delete('countdown_date');
-        $bot->typesAndWaits(5);
-        $bot->replyPrivate('I have removed that event from my memory.');
-    }elseif($diff == 0){
-        $bot->reply($name. ' is today!');
-        $bot->userStorage()->delete('countdown_name');
-        $bot->userStorage()->delete('countdown_date');
-        $bot->typesAndWaits(5);
-        $bot->replyPrivate('I have removed that event from my memory.');
-    }
-});
-
-$botman->hears('/removecountdown@'.env('BOT_NAME'), function(Botman $bot){
-        $bot->userStorage()->delete('countdown_name');
-        $bot->userStorage()->delete('countdown_date');
-        $bot->reply('Done!');
 });
 
 //for now start_conversation and set_intro does the same thing. Change in the future.
