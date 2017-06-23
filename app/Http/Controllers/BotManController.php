a8cf0f842b0af1cfff5d19155d24f80fffc94b9c
xmodulus <azizsafudin@yahoo.com.sg>
2017-06-23 23:37:15 +0800

    Simulating firefox request

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -44,9 +44,15 @@ class BotManController extends Controller
     public function getPrayerTimes($source = 'muis'){
         if($source == 'muis') {
             $url = "http://www.muis.gov.sg/";
+            $agent= 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)';
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_VERBOSE, true);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_USERAGENT, $agent);
+            curl_setopt($ch, CURLOPT_URL,$url);
             $data = curl_exec($ch);
             curl_close($ch);
             $dom = new \DOMDocument();
